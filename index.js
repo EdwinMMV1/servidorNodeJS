91448ec0761e31771b97c413cbb8c5dd0323bdac
EdwinMMV1 <98926339+EdwinMMV1@users.noreply.github.com>
2022-03-08 23:33:31 -0500

    update

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
 import App from './config/app.js';
 
-//const express = require('express');
-//const dotenv = require('dotenv');
-//const app = express();
-
-//app.use(express.urlencoded({ extended: true }));
-//app.use(express.json())
-//dotenv.config();
-
-//const port = process.env.APP_PORT || 8081;
-
-
-/*app.get('/', (req, res) => {
-    res.send("Hola mundo");
-});*/
-
-/*app.post('/data', function(req, res) {
-    const data = req.body;
-    console.log(req.body);
-    res.json({
-        ok: true,
-        message: 'datos recibidos con éxito'
-    });
-});*/
-
-/*app.listen(port, () => {
-    console.log(`server started at http://localhost:${ port }`);
-});*/
-
 const port = process.env.APP_PORT || 8081;
 
 App.app.listen(port, () => console.log(`Api es running ${port}`));
